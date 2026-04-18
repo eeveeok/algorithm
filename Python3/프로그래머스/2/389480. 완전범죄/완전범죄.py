d40803ae10858155c8e93609dbac9087cc9673ce
junhyeok jung <85117182+eeveeok@users.noreply.github.com>
2026-04-18 21:28:36 +0900

    [level 2] Title: 완전범죄, Time: 1.18 ms, Memory: 9.14 MB -BaekjoonHub

--- Python3/프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ Python3/프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,26 +1,29 @@
 def solution(info, n, m):
-    answer = -1
-
-    visited = set()
+    answer = 0
     
-    def dfs(cur = 0, signA = 0, signB = 0):
-        nonlocal answer, n, m
-        
-        if signA >= n or signB >= m:
-            return
-        
-        if (signA, signB, cur) in visited:
-            return 
-        
-        visited.add((signA, signB, cur))
-        
-        if cur == len(info):
-            answer = signA if answer == -1 else min(answer, signA)
-            return
-        
-        dfs(cur + 1, signA + info[cur][0], signB)
-        dfs(cur + 1, signA, signB + info[cur][1])
+    INF = 10**9
+    length = len(info)
     
-    dfs()
+    dp = [[INF] * n for _ in range(length+1)]
+    dp[0][0] = 0
     
-    return answer
+    for i in range(1, length+1):
+        a_i, b_i = info[i-1]
+        for prev_a in range(n):
+            if dp[i-1][prev_a] >= m:
+                continue
+            
+            new_a = prev_a + a_i
+            if new_a < n:
+                dp[i][new_a] = min(dp[i][new_a], dp[i-1][prev_a])
+            
+            new_b = dp[i-1][prev_a] + b_i
+            if new_b < m:
+                dp[i][prev_a] = min(dp[i][prev_a], new_b)
+    
+    answer = INF
+    for i in range(n):
+        if dp[length][i] < m:
+            answer = min(answer, i)
+    
+    return answer if answer != INF else -1
