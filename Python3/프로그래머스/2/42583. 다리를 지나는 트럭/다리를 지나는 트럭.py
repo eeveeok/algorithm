9902dc4c3f003eb0c16e91e2bdbab067b7453009
junhyeok jung <85117182+eeveeok@users.noreply.github.com>
2026-04-20 03:42:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 50.60 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,23 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 1
+    
+    time = 0
+    cur_weights = 0
+    enter_info = []
+    
+    while truck_weights:
+        time += 1
+        
+        w = truck_weights[0]
+        
+        if enter_info and enter_info[0][1] + bridge_length == time:
+            cur_weights -= enter_info.pop(0)[0]
+        
+        if cur_weights + w <= weight:
+            print(time)
+            enter_info.append((w, time))
+            cur_weights += w
+            truck_weights.pop(0)
+    
+    answer = time + bridge_length
+    return answer
