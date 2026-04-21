6fc5b427ca2438880af443dff4b83a28d21c1a39
junhyeok jung <85117182+eeveeok@users.noreply.github.com>
2026-04-21 20:36:52 +0900

    [level 2] Title: 타겟 넘버, Time: 343.90 ms, Memory: 9.08 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/README.md
+++ Python3/프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 394.86 ms
+메모리: 9.08 MB, 시간: 343.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 10일 21:04:24
+2026년 04월 21일 20:36:50
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,11 @@
-def dfs(idx, op, numbers, target):
-    if idx == len(numbers):
+def dfs(cur, op, numbers, target):
+    if cur == len(numbers):
         return 1 if op == target else 0
     
-    return (
-        dfs(idx + 1, op + numbers[idx], numbers, target)
-        + dfs(idx + 1, op - numbers[idx], numbers, target)
-    )
+    nxt = cur + 1;
+    return dfs(nxt, op + numbers[cur], numbers, target) + dfs(nxt, op - numbers[cur], numbers, target)
 
 def solution(numbers, target):
-    answer = 0
-    
-    answer = (dfs(1, numbers[0], numbers, target)
-    + dfs(1, -numbers[0], numbers, target))
+    answer = dfs(1, numbers[0], numbers, target) + dfs(1, -numbers[0], numbers, target)
     
     return answer
