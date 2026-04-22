7200e451af50174dc24c9bd1797c0f9aca6bc56c
junhyeok jung <85117182+eeveeok@users.noreply.github.com>
2026-04-22 09:48:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.43 ms, Memory: 9.46 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,28 @@
 def solution(maps):
     answer = 0
     
-    h = len(maps) + 1
-    w = len(maps[0]) + 1
+    h = len(maps)
+    w = len(maps[0])
     
     queue = []
-    visited = [[-1] * h for _ in range(w)]
+    visited = [[-1] * w for _ in range(h)]
     
     dir_x = [0, 1, 0, -1]
     dir_y = [1, 0, -1, 0]
     
-    queue.append(((1, 1), 1))
+    queue.append((0, 0))
+    visited[0][0] = 1
     while queue:
         pair = queue.pop(0)
-        
-        org_x, org_y = pair[0][0], pair[0][1]
-        step = pair[1]
+        org_x, org_y = pair[0], pair[1]
         
         for i in range(4):
             new_x = org_x + dir_x[i]
             new_y = org_y + dir_y[i]
             
-            if 0 < new_x < h and 0 < new_y < w and maps[new_x-1][new_y-1] == 1 and visited[new_x][new_y] == -1:
-                visited[new_x][new_y] = step+1
-                queue.append(((new_x, new_y), step+1))
+            if 0 <= new_x < h and 0 <= new_y < w and maps[new_x][new_y] == 1 and visited[new_x][new_y] == -1:
+                visited[new_x][new_y] = visited[org_x][org_y] + 1
+                queue.append((new_x, new_y))
     
     answer = visited[-1][-1]
     
